2e9cf6c9af21530d49d960d57360421aa7057aad
tornike2002 <toko.toka2000@gmail.com>
2024-01-11 23:49:02 +0400

    changed h1 to link tag

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,28 +1,44 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div
       className="text-navPrimary text-base justify-center flex items-center border font-Fira h-10
      bg-MainPrimary font-medium border-NavBorderColor rounded-md"
     >
-      <div className="w-1/6 border-r h-full border-NavBorderColor">
-        <h1 className="pl-4 pt-2">tornike-butiashvili</h1>
+      <div className="w-1/6 border-r h-full border-NavBorderColor pt-2">
+        <Link to="/" className="pl-4">
+          tornike-butiashvili
+        </Link>
       </div>
       <div className="flex justify-between items-center w-[100%]">
         <div className="flex gap-10 items-center justify-center text-center h-10">
-          <h1 className="border-r border-NavBorderColor pl-5 pr-5 h-full pt-2">
+          <Link
+            to="/"
+            className="border-r border-NavBorderColor pl-5 pr-5 h-full pt-2"
+          >
             _hello
-          </h1>
-          <h1 className="border-r border-NavBorderColor pr-8 h-full pt-2">
+          </Link>
+          <Link
+            to="/"
+            className="border-r border-NavBorderColor pr-8 h-full pt-2"
+          >
             _about_me
-          </h1>
-          <h1 className="border-r border-NavBorderColor pr-8 h-full pt-2">
+          </Link>
+          <Link
+            to=""
+            className="border-r border-NavBorderColor pr-8 h-full pt-2"
+          >
             _projects
-          </h1>
+          </Link>
         </div>
-        <div className="h-10">
-          <h1 className="pr-4 pl-4 border-l border-NavBorderColor h-full pt-2">
+        <div className="h-10 pt-2">
+          <Link
+            to="/"
+            className="pr-4 pl-4 border-l border-NavBorderColor h-full"
+          >
             _contact_me
-          </h1>
+          </Link>
         </div>
       </div>
     </div>
