88f83bb275d42bd614d37f28d1ef971c6d2a5591
tornike2002 <toko.toka2000@gmail.com>
2024-01-12 16:02:44 +0400

    added text for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,17 @@
-
 const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="min-h-[80vh] text-white">
+      <div>
+        <p>Hi all. I am</p>
+        <h1>Tornike Butiashvili</h1>
+        <h3> -- React Developer</h3>
+      </div>
+      <div>
+        <p>// find my profile on Github:</p>
+        <h1>const githubLink = https://github.com/beqa200</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
