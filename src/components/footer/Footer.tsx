ca9e626831dbcb5de40b365b2973f7c033f2b7ad
tornike2002 <toko.toka2000@gmail.com>
2024-01-13 20:57:57 +0400

    added icons for footer and links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,29 @@
+import { FacebookFilled, LinkedinFilled } from "@ant-design/icons";
+
 const Footer: React.FC = () => {
   return (
     <div className="bg-MainPrimary text-white h-12 border border-NavBorderColor rounded-sm">
-      <div>
-        <h1 className="font-Fira text-base font-medium text-navPrimary">find me in:</h1>
-        <span></span>
-        <span></span>
+      <div className="flex items-center">
+        <h1
+          className="font-Fira text-base font-medium text-navPrimary border h-12 pt-3 px-4
+         border-NavBorderColor"
+        >
+          find me in:
+        </h1>
+        <a
+          target="_blank"
+          href="https://www.facebook.com/Painoglu"
+          className="border h-12 px-4 pt-1 border-NavBorderColor"
+        >
+          <FacebookFilled className="text-2xl text-navPrimary opacity-40" />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/tornike-butiashvili-4b3633226/"
+          className="border h-12 px-4 pt-1 border-NavBorderColor"
+        >
+          <LinkedinFilled className="text-2xl text-navPrimary opacity-40" />
+        </a>
       </div>
     </div>
   );
