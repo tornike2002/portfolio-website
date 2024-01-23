bf1e44d5d20e4970d69970fb450d55d81655a7ff
tornike2002 <toko.toka2000@gmail.com>
2024-01-23 22:23:16 +0400

    added aside elements

--- src/components/about/AboutAside.tsx
+++ src/components/about/AboutAside.tsx
@@ -1,9 +1,18 @@
 import { Fragment } from "react";
-import { DownOutlined } from "@ant-design/icons";
+import {
+  DownOutlined,
+  CaretDownOutlined,
+  FolderOutlined,
+  MailOutlined,
+  PhoneOutlined,
+} from "@ant-design/icons";
 const AboutAside: React.FC = () => {
   return (
     <Fragment>
-      <aside className="text-white font-Fira border rounded-md border-NavBorderColor w-[25%]">
+      <aside
+        className="text-white font-Fira border rounded-md border-NavBorderColor w-[25%]
+       min-h-[80vh]"
+      >
         <div className="pl-4 border-b border-NavBorderColor ">
           <h1 className="p-5">_about-me</h1>
         </div>
@@ -12,10 +21,28 @@ const AboutAside: React.FC = () => {
             <DownOutlined />
             info
           </span>
+          <div className="flex flex-col items-start pl-7 cursor-pointer tracking-wider">
+            <span className="flex items-center gap-2 font-bold">
+              <CaretDownOutlined className="cursor-pointer" />
+              <FolderOutlined className="cursor-pointer" /> Personal
+            </span>
+            <span className="flex items-center gap-2 cursor-pointer font-bold tracking-wider">
+              <CaretDownOutlined className="cursor-pointer" />
+              <FolderOutlined className="cursor-pointer" /> Proffesional
+            </span>
+          </div>
           <span className="flex items-center gap-1 pl-5 bg-NavBorderColor">
             <DownOutlined />
             contacts
           </span>
+          <div>
+            <h1 className="flex items-center gap-3 pl-5 text-navPrimary font-medium tracking-wider">
+              <MailOutlined /> butiashvilitornike18@gmail.com
+            </h1>
+            <h1 className="flex items-center gap-3 pl-5 text-navPrimary font-medium tracking-wider">
+              <PhoneOutlined /> (+995) 598 78 83 00
+            </h1>
+          </div>
         </div>
       </aside>
     </Fragment>
