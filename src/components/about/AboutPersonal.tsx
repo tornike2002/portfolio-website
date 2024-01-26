bcfef57948930035c3afb3a470fde5e3c4bc6984
tornike2002 <toko.toka2000@gmail.com>
2024-01-26 21:24:56 +0400

    added personal text for about section

---
+++ src/components/about/AboutPersonal.tsx
@@ -0,0 +1,21 @@
+const AboutPersonal: React.FC = () => {
+  return (
+    <div>
+      <h1>// info / personal</h1>
+      <p>
+        /* Hey there! I'm Tornike Butiashvili, a 21-year-old individual hailing from
+        the captivating city of Rustavi, Georgia. I consider myself a spirited
+        and goal-driven person, consistently working towards my objectives with
+        zeal. Fueled by unwavering determination, I actively pursue my
+        aspirations, ensuring that I stay focused on the path to success. As
+        proof of my dedication, I'm thrilled to share that I've achieved this
+        level of proficiency in just a few short months. I warmly invite you to
+        explore my diverse portfolio and skills, offering a vivid snapshot of my
+        capabilities and the exciting journey that has brought me to where I am
+        today. */
+      </p>
+    </div>
+  );
+};
+
+export default AboutPersonal;
