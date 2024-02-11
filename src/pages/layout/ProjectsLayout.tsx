6a3e99cd8d606aa6eb1ceb1b731bf680396adfa1
tornike2002 <toko.toka2000@gmail.com>
2024-02-11 18:17:34 +0400

    added sidebar for projects component

--- src/pages/layout/ProjectsLayout.tsx
+++ src/pages/layout/ProjectsLayout.tsx
@@ -1,10 +1,12 @@
 import Footer from "../../components/footer/Footer";
 import NavbarPage from "../navbar/NavbarPage";
+import ProjectsPage from "../projects/ProjectsPage";
 
 const ProjectsLayout: React.FC = () => {
   return (
     <div className="min-h-screen max-w-full p-11 m-auto">
       <NavbarPage />
+      <ProjectsPage />
       <Footer />
     </div>
   );
---
+++ src/pages/projects/ProjectsPage.tsx
@@ -0,0 +1,11 @@
+import ProjectsSidebar from "../../components/projects/ProjectsSidebar";
+
+const ProjectsPage: React.FC = () => {
+  return (
+    <div>
+      <ProjectsSidebar />
+    </div>
+  );
+};
+
+export default ProjectsPage;
